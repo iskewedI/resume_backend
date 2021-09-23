e8414f555f3073d40d2dfb420fed726f8b405adf
iskewedi <jtornello2019@hotmail.com>
2021-09-23 19:02:12 -0300

    Added startup functions

---
+++ startup/routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const projectsRoute = require('../routes/projectsRoute');
+const error = require('../middleware/error');
+const cors = require('../middleware/cors');
+const { apiVersion, endpoints } = require('../config.json');
+
+const getApiEndpoint = endpoint => `/${apiVersion}/${endpoint}`;
+
+module.exports = function (app) {
+  app.use(express.json());
+
+  app.use(cors);
+
+  app.use(getApiEndpoint(endpoints.Projects), projectsRoute);
+
+  //Error Middleware, always LAST middleware used
+  app.use(error);
+};
