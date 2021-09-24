7df4bda0af097e56862ac0ae359ee2e2abc4758f
iskewedi <jtornello2019@hotmail.com>
2021-09-24 10:25:06 -0300

    Changed CORS middleware policies

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,12 +1,12 @@
 //Authorization middleware
 module.exports = function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://iskewedi.github.io/');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, x-auth-token'
+    'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
   );
-  res.setHeader('Access-Control-Expose-Headers', 'x-auth-token');
+
   next();
 };
